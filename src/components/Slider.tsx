c1cb13316a55160b58cca72c615c9286a4424d1b
unknown <ashwininagargoje703@gmail.com>
2023-12-06 13:09:10 +0530

    update

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -5,23 +5,23 @@ const Slider: React.FC = () => {
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 m-4">
       <Carousel>
         <img
-          src="https://images.vexels.com/content/194700/preview/buy-online-slider-template-4261dd.png"
+          src="https://cdn.vectorstock.com/i/preview-1x/14/46/isometric-online-shopping-vector-35151446.jpg"
           alt="..."
         />
         <img
-          src="https://4.imimg.com/data4/TX/JE/GLADMIN-30141012/wp-content-uploads-2016-05-indiabbazaar-e-commerce.jpg"
+          src="https://static.vecteezy.com/system/resources/previews/001/184/072/original/online-smartphone-shopping-banner-vector.jpg"
           alt="..."
         />
         <img
-          src="https://cdn.sanity.io/images/cbjxg0yl/production_v2/207796ae5c4b77588415a94bdaf3c8c29ea92bd3-2480x761.jpg"
+          src="https://img.freepik.com/free-vector/online-shopping-banner-template_23-2148787138.jpg"
           alt="..."
         />
         <img
-          src="https://i.pinimg.com/736x/b6/c9/17/b6c9173bd58f62f49eb550635a5e259f.jpg"
+          src="https://img.freepik.com/free-vector/online-shopping-banner-template_23-2148896886.jpg"
           alt="..."
         />
         <img
-          src="https://cdn.youcan.shop/stores/7d219d104086fbd2654a1f045d0f635c/others/8sxLyLbaoB4s0uRNN8BsiqZLgfhOkNCfC34aMbpi.png"
+          src="https://t3.ftcdn.net/jpg/04/31/70/04/360_F_431700457_pLjYX4hAuDhb2bMxFL0805UA86IgEX9t.jpg"
           alt="..."
         />
       </Carousel>
